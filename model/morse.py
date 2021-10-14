58c88c99c94bdf314aa7855aba8690fea5b2b877
Lexus Wang <gazhijj@qq.com>
2021-10-14 09:58:46 -0500

    detection finished

--- model/morse.py
+++ model/morse.py
@@ -79,116 +79,6 @@ class Morse:
     def backward(self):
         pass
 
-
-    # def forward(self, learn):
-    #     # feed forward, get the predicted result
-
-    #     # generate sequence
-
-
-    #     simple_net_final_grad_of_multiple_batches = []
-    #     final_morse_grad_of_multiple_batches = []
-    #     forward_result_batches = []
-
-    #     for node_id in self.data_loader:
-    #         node = self.data_loader[node_id]
-    #         [sequence, morse_grad, simple_net_grad] = node.generate_sequence_and_grad(self.batch_size,
-    #                                                                                   self.sequence_size)
-    #         # sequence: (?, 5, 12)
-    #         # morse_grad: (?, 5, 12, 2)
-    #         # simple_net_grad: (?, 5, 12, 4)
-
-    #         need = self.batch_size - self.cur_len
-
-    #         if len(sequence) + self.cur_len > self.batch_size:
-    #             self.cur_batch += sequence[:need]
-    #             self.cur_simple_net_grad_list += simple_net_grad[:need]
-    #             self.cur_morse_grad_list += morse_grad[:need]
-    #             self.cur_len = self.batch_size
-    #             self.cur_event_type_list += node.event_type_list[:need]
-    #             self.cur_event_list += node.event_list[:need]
-
-    #             remain_batch = sequence[need:]
-    #             remain_event_list = node.event_list[need:]
-    #             remain_event_type_list = node.event_type_list[need:]
-    #             remain_simple_net_grad_list = simple_net_grad[need:]
-    #             remain_morse_grad_list = morse_grad[need:]
-    #         else:
-    #             self.cur_batch += sequence[:need]
-    #             self.cur_morse_grad_list += morse_grad[:need]
-    #             self.cur_simple_net_grad_list += simple_net_grad[:need]
-    #             self.cur_len += len(sequence)
-    #             self.cur_event_type_list += node.event_type_list[:need]
-    #             self.cur_event_list += node.event_list[:need]
-    #         # batch_size * sequence_size * feature _size
-    #         # batch_size: 100
-    #         # sequence_size: 5
-    #         # feature_size: 12
-    #         # 100*5*12
-
-    #         # print("creating batch")
-    #         if self.cur_len >= self.batch_size:
-    #             # print("cur_batch, size: ", np.shape(cur_batch), len(cur_batch[0][0]), cur_batch)
-    #             input_tensor = torch.tensor(self.cur_batch)
-    #             self.simple_net_grad_tensor = torch.tensor(self.cur_simple_net_grad_list, dtype=torch.float)
-    #             self.morse_grad_tensor = torch.tensor(self.cur_morse_grad_list, dtype=torch.float)
-
-    #             # input_tensor: (100, 5, 12)
-    #             # morse_grad_tensor: (100, 5, 12, 2)
-    #             # simple_net_grad_tensor: (100, 5, 12, 4)
-
-    #             # print("getting into RNN")
-
-    #             # input size: 100 * 5 * 12
-    #             # output size: 100 * 5 * 12
-
-    #             # final_grad = morse_train.back_propagate(input_tensor, event_type_list, event_list, rnn_grad)
-    #             # print(final_grad)
-    #             # # update weights
-    #             # self.a_b_setter(-learn * final_grad[0])
-    #             # self.a_e_setter(-learn * final_grad[1])
-    #             # self.benign_thresh_model_setter(final_grad[2])
-    #             # self.suspect_env_model_setter(final_grad[3])
-
-    #             self.cur_batch = self.remain_batch[::]
-    #             self.cur_morse_grad_list = self.remain_morse_grad_list[::]
-    #             self.cur_simple_net_grad_list = self.remain_simple_net_grad_list[::]
-    #             self.cur_event_type_list = self.remain_event_type_list[::]
-    #             self.cur_event_list = self.remain_event_list[::]
-    #             self.cur_len = len(self.cur_batch)
-
-    #             self.remain_batch = []
-    #             self.remain_event_list = []
-    #             self.remain_event_type_list = []
-    #             self.remain_morse_grad_list = []
-    #             self.remain_simple_net_grad_list = []
-
-    #             forward_result_batches.append(input_tensor)
-    #             # return input_tensor
-
-    #             # print(type(simple_net_grad_tensor), type(rnn_grad))
-    #             # calculate the final grads of loss wrt w,b in simple_net by
-    #             # combining grads from simple_net and grads from rnn
-
-    #             # print(rnn_grad.is_cuda)
-    #             # print(simple_net_grad_tensor.is_cuda)
-
-    #     # self.data_loader.pos = 0
-    #     return forward_result_batches
-
-
-    #     # if len(simple_net_final_grad_of_multiple_batches) > 0:
-    #     #     average_final_simplenet_grads = sum(simple_net_final_grad_of_multiple_batches) / len(
-    #     #         simple_net_final_grad_of_multiple_batches)
-    #     #     average_final_morse_grads = sum(final_morse_grad_of_multiple_batches) / len(
-    #     #         final_morse_grad_of_multiple_batches)
-    #     #     self.a_b_setter(-learn * average_final_morse_grads[0])
-    #     #     self.a_e_setter(-learn * average_final_morse_grads[1])
-    #     #
-    #     #     # update SimpleNet's weights
-    #     #     self.benign_thresh_model_setter(average_final_simplenet_grads[0], average_final_simplenet_grads[1])
-    #     #     self.suspect_env_model_setter(average_final_simplenet_grads[2], average_final_simplenet_grads[3])
-
     # -------------- tag getters ------------------ #
 
     def get_benign_thresh(self) -> float:
@@ -329,7 +219,9 @@ class Morse:
 
     def add_object(self, object_node, object):
         self.G.add_node(object_node['uuid'])
-        initObjectTags(object, self.obj_inits, format=self.format)
+        # initObjectTags(object, self.obj_inits, format=self.format)
+        a = self.node_inital_tags[object_node['uuid']].tolist()
+        object.setObjTags(self.node_inital_tags[object_node['uuid']].tolist()[0])
         self.Nodes[object_node['uuid']] = object
 
     def add_subject(self, subject_node, subject):
@@ -337,7 +229,9 @@ class Morse:
         if subject.pid in self.processes and self.processes[subject.pid]['alive']:
             subject.setSubjTags(self.Nodes[self.processes[subject.pid]['node']].tags())
         else:
-            initSubjectTags(subject, self.subj_init)
+            # initSubjectTags(subject, self.subj_init)
+            a = self.node_inital_tags[subject_node['uuid']].tolist()
+            subject.setSubjTags(self.node_inital_tags[subject_node['uuid']].tolist()[0])
         self.processes[subject.pid] = {}
         self.processes[subject.pid]['node'] = subject_node['uuid']
         self.processes[subject.pid]['alive'] = True
--- train.py
+++ train.py
@@ -16,40 +16,6 @@ import torch
 from model.morse import Morse
 from utils.Initializer import Initializer, FileObj_Initializer, NetFlowObj_Initializer
 
-class MORSE(torch.autograd.Function):
-    """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
-    """
-
-    @staticmethod
-    def forward(ctx, input):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation. You can cache arbitrary
-        objects for use in the backward pass using the ctx.save_for_backward method.
-        """
-        ctx.save_for_backward(input)
-        return 0.5 * (5 * input ** 3 - 3 * input)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-        """
-        input, = ctx.saved_tensors
-        if condition == True:
-            return grad_output * 1
-        else:
-            return grad_output * -1
-
-
-
-
 def get_node_features(file):
     null = 0
     node_features = {}
@@ -113,16 +79,19 @@ def parse_logs(file):
         feature_array = target_features['features'].values.tolist()
         feature_array = torch.tensor(feature_array)
         tags = node_inits[node_type].initialize(feature_array)
-        for node_id in target_features['features'].index.tolist():
-            node_inital_tags[node_id] = tags
+        for i, node_id in enumerate(target_features.index.tolist()):
+            node_inital_tags[node_id] = tags[i,:]
 
-    node_type == 'Subject'
+    node_type = 'Subject'
     target_features = df[df['type']==node_type]
     feature_array = [[0] for i in range(len(target_features))]
     feature_array = torch.tensor(feature_array)
     tags = node_inits[node_type].initialize(feature_array)
-    for node_id in target_features['features'].index.tolist():
-        node_inital_tags[node_id] = tags
+    for i, node_id in enumerate(target_features.index.tolist()):
+        node_inital_tags[node_id] = tags[i,:]
+
+
+    mo.node_inital_tags = node_inital_tags
 
 
     # for i in range(7):
@@ -185,6 +154,12 @@ def parse_logs(file):
                     pass
 
     # ============= Backward & Update =================== #
+    loss = None
+    optimizer = None
+    loss.backward()
+
+    optimizer.step()
+
 
     
 
