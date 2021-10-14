674f7272ed9a9baab4610a86319ec1d042bf8938
WEIJIAN LI <weijianli@WEIJIANs-MacBook-Pro-2.local>
2021-10-14 14:06:59 -0500

    add more event types to loss

--- model/loss.py
+++ model/loss.py
@@ -17,25 +17,33 @@ def get_loss(event_type: int, s: torch.Tensor, o: torch.Tensor, origtags, alarm_
 
     if side == "false_positive":
         if event_type == standard_events['EVENT_EXECUTE'] or event_type == standard_events['EVENT_LOADLIBRARY']:
-            if citag(origtags) == TRUSTED and citag(s.tags()) == UNTRUSTED:
-                s_loss = s - torch.tensor([TRUSTED, 0, 0, 0, 0])
+            s_loss = s - torch.tensor([TRUSTED, 0, 0, 0, 0])
+
+        elif event_type == standard_events['EVENT_MODIFY_PROCESS']:
+            o_loss = o - torch.tensor([TRUSTED, 0, 0, 0, 0])
 
         elif event_type == standard_events['EVENT_WRITE']:
-            if (o.isIP() and not o.isMatch("UnknownObject") and not o.isMatch("Pipe[") and not o.isMatch(
-                    "pipe") and not o.isMatch("null") and itag(origtags) > 0.5 and itag(o.tags()) <= 0.5):
-                # to be discussed: which to be chosen to optimized (one is enough, more is also ok)
-                o_loss = o - torch.tensor([0, 0, 0, np.random.normal(0.75, 1), 0])
-                if alarm_name == "DataLeak":
-                    s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.75, 1), np.random.normal(0.75, 1)])
+            # to be discussed: which to be chosen to optimized (one is enough, more is also ok)
+            o_loss = o - torch.tensor([0, 0, 0, np.random.normal(0.75, 1), 0])
+            if alarm_name == "DataLeak":
+                s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.75, 1), np.random.normal(0.75, 1)])
+
+        elif event_type == standard_events['EVENT_CHANGE_PRINCIPAL']:
+            s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.75, 1), 0])
 
     elif side == "false_negative":
         if event_type == standard_events['EVENT_EXECUTE'] or event_type == standard_events['EVENT_LOADLIBRARY']:
             s_loss = s - torch.tensor([UNTRUSTED, 0, 0, 0, 0])
+
+        elif event_type == standard_events['EVENT_MODIFY_PROCESS']:
+            o_loss = o - torch.tensor([UNTRUSTED, 0, 0, 0, 0])
+
         elif event_type == standard_events['EVENT_WRITE']:
-            if (o.isIP() and not o.isMatch("UnknownObject") and not o.isMatch("Pipe[") and not o.isMatch(
-                    "pipe") and not o.isMatch("null")):
-                o_loss = o - torch.tensor([0, 0, 0, np.random.normal(0.25, 1), 0])
-                if alarm_name == "DataLeak":
-                    s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.25, 1), np.random.normal(0.25, 1)])
+            o_loss = o - torch.tensor([0, 0, 0, np.random.normal(0.25, 1), 0])
+            if alarm_name == "DataLeak":
+                s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.25, 1), np.random.normal(0.25, 1)])
+
+        elif event_type == standard_events['EVENT_CHANGE_PRINCIPAL']:
+            s_loss = s - torch.tensor([0, 0, 0, np.random.normal(0.25, 1), 0])
 
     return torch.mean(s_loss), torch.mean(o_loss)
