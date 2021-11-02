5846582ef195d8f986ef360366784fd793520d32
Xiangmin Shen <Xiangminshen2019@u.northwestern.edu>
2021-11-02 18:35:11 -0500

    added support for reading from and writing to multiple files

--- utils/dataProcessor.py
+++ utils/dataProcessor.py
@@ -1,25 +1,32 @@
 import json
 import configparser
 import os
+import glob
 
 class dataProcessor:
     def __init__(self, configFile):
         self.config = configparser.ConfigParser()
         self.config.read(configFile)
         self.dpConfig = self.config['DataProcessor']
-        self.fromPath = self.dpConfig['FromPath']
+        self.fromFormat = self.dpConfig['FromFileFormat']
         self.toDir = self.dpConfig['ToDir']
-        self.vertexDataFileName = self.dpConfig['vertexDataFileName']
-        self.edgeDataFileName = self.dpConfig['edgeDataFileName']
+        self.vertexDataFileName = self.dpConfig['VertexDataFileName']
+        self.edgeDataFileName = self.dpConfig['EdgeDataFileName']
+        self.segSize = int(self.dpConfig['SegmentSize'])
 
     def separate(self):
         if not os.path.exists(self.toDir):
             os.makedirs(self.toDir)
         vertexPath = os.path.join(self.toDir, self.vertexDataFileName)
-        edgePath = os.path.join(self.toDir, self.edgeDataFileName)
-        with open(self.fromPath, 'r') as f:
-            with open(vertexPath, 'w+') as v:
-                with open(edgePath, 'w+') as e:
+        counter = 0
+        edgeIndex = 0
+        edgePath = os.path.join(self.toDir, self.edgeDataFileName + '.' + str(edgeIndex))
+        files = glob.glob(self.fromFormat)
+        print(files)
+        e = open(edgePath, 'w+')
+        with open(vertexPath, 'w+') as v:
+            for file in sorted(files):
+                with open(file, 'r') as f:
                     for line in f:
                         tmp = json.loads(line.strip())
                         if 'com.bbn.tc.schema.avro.cdm18.Subject' in tmp['datum'] or \
@@ -30,3 +37,10 @@ class dataProcessor:
                             v.write(line)
                         elif 'com.bbn.tc.schema.avro.cdm18.Event' in tmp['datum']:
                             e.write(line)
+                            counter += 1
+                            if counter == self.segSize:
+                                e.close()
+                                edgeIndex += 1
+                                counter = 0
+                                edgePath = os.path.join(self.toDir, self.edgeDataFileName + '.' + str(edgeIndex))
+                                e = open(edgePath, 'w+')
