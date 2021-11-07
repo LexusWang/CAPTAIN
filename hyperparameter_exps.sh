dc87d6532dc205f7d7d85a29b9a3f518539c9813
leevisir <weijianli@WEIJIANs-MacBook-Pro-2.local>
2021-11-07 12:33:28 -0600

    debug

--- hyperparameter_exps.sh
+++ hyperparameter_exps.sh
@@ -1,8 +1,8 @@
 for LR in 0.1 0.01 0.001 0.0001
 do
-  for EPOCH in 5 10 20
+  for EPOCH in 5 10
   do
-    for LR_IMB in 1 2 4 8 16
+    for LR_IMB in 2 4 16
     do
         python start_single_experiments.py --learning_rate ${LR} --epoch ${EPOCH} --lr_imb ${LR_IMB} &
         sleep 1s
