61f3ccd73f50694cf03a29bf5bf2f1bccfd51f06
Xiangmin Shen <Xiangminshen2019@u.northwestern.edu>
2021-12-07 13:37:18 -0600

    updated eventClassifier to provide more info about the missing TP

--- eventClassifierTest.py
+++ eventClassifierTest.py
@@ -1,22 +1,23 @@
 from utils.eventClassifier import eventClassifier
 
-ec = eventClassifier('groundTruth.txt')
+ec = eventClassifier('groundTruth32.txt')
 if ec.classify('0F8CF745-900F-206D-F134-28686757C4D5') == "DataLeak":
     print("correctly classified DataLeak Alarm")
 else:
     print("error")
 
-if ec.classify('DEE900D6-80D1-662E-402E-1137BDF25420') == "FileExec":
-    print("correctly classified FileExec Alarm")
-else:
-    print("error")
+# if ec.classify('DEE900D6-80D1-662E-402E-1137BDF25420') == "FileExec":
+#     print("correctly classified FileExec Alarm")
+# else:
+#     print("error")
 
-if ec.classify('A5DA3328-1DB8-EDDD-4637-60CCE0FEFE3E') == "MkFileExecutable":
-    print("correctly classified MkFileExecutable Alarm")
-else:
-    print("error")
+# if ec.classify('A5DA3328-1DB8-EDDD-4637-60CCE0FEFE3E') == "MkFileExecutable":
+#     print("correctly classified MkFileExecutable Alarm")
+# else:
+#     print("error")
 
-if ec.classify('60C3AC79-466B-5C25-B2AF-AEC19A5CFC77') == "MkMemExecutable":
-    print("correctly classified MkMemExecutable Alarm")
-else:
-    print("error")
+# if ec.classify('60C3AC79-466B-5C25-B2AF-AEC19A5CFC77') == "MkMemExecutable":
+#     print("correctly classified MkMemExecutable Alarm")
+# else:
+#     print("error")
+ec.summary()
