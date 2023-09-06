e42c61e015433071c05f48effa5d837f4ed9da12
Lexus Wang <gazhijj@qq.com>
2023-09-06 21:58:44 +0000

    modified script

--- run_test.sh
+++ run_test.sh
@@ -4,9 +4,9 @@
 # python standard_data-trace.py --input_data ../data/raw/E3-trace-1 --output_data ../data/T31 --format trace --cdm_version 18
 # python standard_data-trace.py --input_data ../data/raw/E3-trace-2 --output_data ../data/T32 --format trace --cdm_version 18
 
-# TRACE
-# python train_by_benign.py --ground_truth_file ../data/GT/groundTruthT31.txt --train_data ../data/T31 --mode train --data_tag t31-train --experiment_prefix TrainT31 --time_range 2018-4-2T00:00:00-04:00 2018-4-10T00:00:00-04:00
-
+# TRACE Train
+python train_by_benign.py --ground_truth_file ../data/GT/groundTruthT31.txt --train_data ../data/T31 --mode train --data_tag t31-train --experiment_prefix TrainT31 --time_range 2018-4-2T00:00:00-04:00 2018-4-10T00:00:00-04:00
+# TRACE Test
 python train_by_benign.py --ground_truth_file ../data/GT/groundTruthT31.txt --train_data ../data/T31 --mode test --data_tag t31-test --experiment_prefix TestT31 --time_range 2018-4-10T00:00:00-04:00 2018-4-13T14:00:00-04:00
 python train_by_benign.py --ground_truth_file ../data/GT/groundTruthT32.txt --train_data ../data/T32 --mode test --data_tag t32-test --experiment_prefix TestT32
 
