79e81a4837809ba686d27610c2c6195f20714949
Lexus Wang <gazhijj@qq.com>
2023-09-15 21:46:16 +0000

    9.15

--- train_by_benign.py
+++ train_by_benign.py
@@ -49,17 +49,13 @@ def start_experiment(args):
             with open(pre_loaded_path, 'rb') as f:
                 events, nodes, principals = pickle.load(f)
         else:
-            events = read_events_from_files(os.path.join(args.train_data, 'edges.json'), args.time_range)
-            nodes = pd.read_json(os.path.join(args.train_data, 'nodes.json'), lines=True).set_index('id').to_dict(orient='index')
-            principals = pd.read_json(os.path.join(args.train_data, 'principals.json'), lines=True).set_index('uuid').to_dict(orient='index')
+            events = read_events_from_files(os.path.join(args.data_path, 'edges.json'), args.time_range)
+            nodes = pd.read_json(os.path.join(args.data_path, 'nodes.json'), lines=True).set_index('id').to_dict(orient='index')
+            principals = pd.read_json(os.path.join(args.data_path, 'principals.json'), lines=True).set_index('uuid').to_dict(orient='index')
             # cache the loaded morse and events for next run
             with open(os.path.join(pre_loaded_path, 'morse.pkl'), "wb") as f:
                 pickle.dump([events, nodes, principals], f)
 
-        # #edge tuning cadets
-        # mo.white_name_set = {'207.46.73.59', '127.0.0.1', '128.55.12.10', '128.55.12.118', '83.150.97.73', '128.55.12.67', '216.87.162.115', '128.55.12.55', '207.25.80.123', '10.0.6.9', '128.55.12.166', '69.20.49.234', '128.55.12.167', '207.46.73.60', '212.60.66.243', '193.40.5.73', '128.55.12.110', '212.190.125.38', '162.99.3.50', '194.90.181.242', '66.252.21.131', '128.55.12.56'}
-        # #edge tuning trace
-        # mo.white_name_set = {'128.55.12.73', '128.55.12.10', '158.28.238.9', '64.86.71.27', '204.2.179.67', '8.15.32.34', '64.4.125.136', '10.0.4.1', '83.222.15.109', '216.163.248.17', '64.191.208.114', '12.149.161.245', '208.17.90.10', '67.28.122.168', '128.55.12.122', '209.132.177.50', '66.252.21.131', '65.214.39.18', '129.33.46.231', '162.97.114.199', '216.9.245.101', '203.192.141.18', '208.75.170.1', '0.0.0.0'}
         lambda_tuning_step = {}
         alpha_tuning_step = {}
         tau_tuning_step = {}
@@ -131,8 +127,9 @@ def start_experiment(args):
             pc_event_counter = Counter()
             for item in propagation_chains:
                 pc_event_counter.update(item)
-            threshold = 100
-            filtered_pc_event_counter = {key: value for key, value in pc_event_counter.items() if value > threshold}
+            threshold = 0.5
+            filtered_pc_event_counter = {key: value for key, value in sorted(pc_event_counter.items(), key = lambda x:x[1], reverse = True)[:int(threshold * len(pc_event_counter))]}
+            # pdb.set_trace()
 
             # for key, value in filtered_pc_event_counter.items():
             #     if key in mo.lambda_dict:
@@ -156,50 +153,49 @@ def start_experiment(args):
                 if key not in filtered_pc_event_counter:
                     mo.lambda_dict[key] = mo.lambda_dict[key] - lambda_tuning_step[key]
                     lambda_tuning_step[key] = 0.5 * lambda_tuning_step[key]
-            # print(mo.lambda_dict)
 
-            # Tune Alpha
-            benign_nid_labels = {}
-            public_nid_labels = {}
-            for item in node_gradients:
-                if item[0][1] == 'i':
-                    if item[0] not in benign_nid_labels:
-                        benign_nid_labels[item[0]] = []
-                    benign_nid_labels[item[0]].append(item[1])
-                elif item[0][1] == 'c':
-                    if item[0] not in public_nid_labels:
-                        public_nid_labels[item[0]] = []
-                    public_nid_labels[item[0]].append(item[1])
+            # # Tune Alpha
+            # benign_nid_labels = {}
+            # public_nid_labels = {}
+            # for item in node_gradients:
+            #     if item[0][1] == 'i':
+            #         if item[0] not in benign_nid_labels:
+            #             benign_nid_labels[item[0]] = []
+            #         benign_nid_labels[item[0]].append(item[1])
+            #     elif item[0][1] == 'c':
+            #         if item[0] not in public_nid_labels:
+            #             public_nid_labels[item[0]] = []
+            #         public_nid_labels[item[0]].append(item[1])
                 
-            benign_node_dict = {}
-            for node, value in benign_nid_labels.items():
-                if mo.Nodes[node[0]].get_name() not in benign_node_dict:
-                    benign_node_dict[mo.Nodes[node[0]].get_name()] = []
-                benign_node_dict[mo.Nodes[node[0]].get_name()].extend(value)
+            # benign_node_dict = {}
+            # for node, value in benign_nid_labels.items():
+            #     if mo.Nodes[node[0]].get_name() not in benign_node_dict:
+            #         benign_node_dict[mo.Nodes[node[0]].get_name()] = []
+            #     benign_node_dict[mo.Nodes[node[0]].get_name()].extend(value)
 
-            public_node_dict = {}
-            for node, value in public_nid_labels.items():
-                if mo.Nodes[node[0]].get_name() not in public_node_dict:
-                    public_node_dict[mo.Nodes[node[0]].get_name()] = []
-                public_node_dict[mo.Nodes[node[0]].get_name()].extend(value)
+            # public_node_dict = {}
+            # for node, value in public_nid_labels.items():
+            #     if mo.Nodes[node[0]].get_name() not in public_node_dict:
+            #         public_node_dict[mo.Nodes[node[0]].get_name()] = []
+            #     public_node_dict[mo.Nodes[node[0]].get_name()].extend(value)
+
+            # # for key, item in benign_node_dict.items():
+            # #     if len(item) > 10 and sum(item)/len(item) > 0.9:
+            # #         mo.white_name_set.add(key)
 
             # for key, item in benign_node_dict.items():
             #     if len(item) > 10 and sum(item)/len(item) > 0.9:
-            #         mo.white_name_set.add(key)
+            #         if key not in mo.alpha_dict:
+            #             mo.alpha_dict[key] = 0.5
+            #             alpha_tuning_step[key] = 0.25
+            #         else:
+            #             mo.alpha_dict[key] = mo.alpha_dict[key] + alpha_tuning_step[key]
+            #             alpha_tuning_step[key] = 0.5 * alpha_tuning_step[key]
 
-            for key, item in benign_node_dict.items():
-                if len(item) > 10 and sum(item)/len(item) > 0.9:
-                    if key not in mo.alpha_dict:
-                        mo.alpha_dict[key] = 0.5
-                        alpha_tuning_step[key] = 0.25
-                    else:
-                        mo.alpha_dict[key] = mo.alpha_dict[key] + alpha_tuning_step[key]
-                        alpha_tuning_step[key] = 0.5 * alpha_tuning_step[key]
-
-            for key in mo.alpha_dict.keys():
-                if key not in benign_node_dict:
-                    mo.alpha_dict[key] = mo.alpha_dict[key] - alpha_tuning_step[key]
-                    alpha_tuning_step[key] = 0.5 * alpha_tuning_step[key]
+            # for key in mo.alpha_dict.keys():
+            #     if key not in benign_node_dict:
+            #         mo.alpha_dict[key] = mo.alpha_dict[key] - alpha_tuning_step[key]
+            #         alpha_tuning_step[key] = 0.5 * alpha_tuning_step[key]
 
             # Tune tau
             # mo.adjust_tau(fp_counter)
@@ -257,36 +253,23 @@ def start_experiment(args):
             with open(pre_loaded_path, 'rb') as f:
                 events, nodes, princicals = pickle.load(f)
         else:
-            events = read_events_from_files(os.path.join(args.train_data, 'edges.json'), args.time_range)
-            nodes = pd.read_json(os.path.join(args.train_data, 'nodes.json'), lines=True).set_index('id').to_dict(orient='index')
-            princicals = pd.read_json(os.path.join(args.train_data, 'principals.json'), lines=True).set_index('uuid').to_dict(orient='index')
+            events = read_events_from_files(os.path.join(args.data_path, 'edges.json'), args.time_range)
+            nodes = pd.read_json(os.path.join(args.data_path, 'nodes.json'), lines=True).set_index('id').to_dict(orient='index')
+            princicals = pd.read_json(os.path.join(args.data_path, 'principals.json'), lines=True).set_index('uuid').to_dict(orient='index')
             # cache the loaded morse and events for next run
             with open(os.path.join(pre_loaded_path, 'morse.pkl'), "wb") as f:
                 pickle.dump([events, nodes, princicals], f)
 
         mo.Principals = princicals
 
-        param_path = 'experiments/TrainL132023-09-12-03-20-48'
+        param_path = 'experiments/ALTrainT312023-09-13-03-08-28'
 
-        with open(os.path.join(param_path+'/train', 'params/lambda.pickle'), 'rb') as fin:
+        with open(os.path.join(args.param_path, 'train', 'params/lambda.pickle'), 'rb') as fin:
             mo.lambda_dict = pickle.load(fin)
-        with open(os.path.join(param_path+'/train', 'params/tau.pickle'), 'rb') as fin:
+        with open(os.path.join(args.param_path, 'train', 'params/tau.pickle'), 'rb') as fin:
             mo.tau_dict = pickle.load(fin)
-        with open(os.path.join(param_path+'/train', 'params/alpha.pickle'), 'rb') as fin:
+        with open(os.path.join(args.param_path, 'train', 'params/alpha.pickle'), 'rb') as fin:
             mo.alpha_dict = pickle.load(fin)
-        # # Cadets
-        # mo.white_name_set = {'207.46.73.59', '127.0.0.1', '128.55.12.10', '128.55.12.118', '83.150.97.73', '128.55.12.67', '216.87.162.115', '128.55.12.55', '207.25.80.123', '10.0.6.9', '128.55.12.166', '69.20.49.234', '128.55.12.167', '207.46.73.60', '212.60.66.243', '193.40.5.73', '128.55.12.110', '212.190.125.38', '162.99.3.50', '194.90.181.242', '66.252.21.131', '128.55.12.56'}
-        # Trace
-        # mo.white_name_set = {'128.55.12.73', '128.55.12.10', '158.28.238.9', '64.86.71.27', '204.2.179.67', '8.15.32.34', '64.4.125.136', '10.0.4.1', '83.222.15.109', '216.163.248.17', '64.191.208.114', '12.149.161.245', '208.17.90.10', '67.28.122.168', '128.55.12.122', '209.132.177.50', '66.252.21.131', '65.214.39.18', '129.33.46.231', '162.97.114.199', '216.9.245.101', '203.192.141.18', '208.75.170.1', '0.0.0.0'}
-        
-        # Linux L11
-        # mo.white_name_set = {'/usr/libexec/grepconf.sh'}
-        
-        # # Linux L12
-        # mo.white_name_set = {'192.168.20.56', '18.225.36.18', '61.75.63.184', '/tmp/atScript/atomic-red-team-Gray_dev1.0/execution-frameworks/contrib/python/Python-3.5.0/conftest', '/data/cs/opt/script/check_service.sh', '/usr/libexec/grepconf.sh'}
-
-        # # Linux L13
-        # mo.white_name_set = {'/usr/libexec/grepconf.sh', '/opt/threatbook/OneAV/oneav/script/install/oneav_service_monitor.sh', '/titan/agent/diag_agent.sh'}
 
         false_alarms = []
         for event in tqdm.tqdm(events):
@@ -335,13 +318,14 @@ def start_experiment(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="train or test the model")
     parser.add_argument("--ground_truth_file", type=str)
-    parser.add_argument("--train_data", type=str)
-    parser.add_argument("--test_data", type=str)
+    parser.add_argument("--data_path", type=str)
+    # parser.add_argument("--test_data", type=str)
     parser.add_argument("--epoch", default=10, type=int)
     parser.add_argument("--mode", type=str)
     parser.add_argument("--data_tag", type=str)
     parser.add_argument("--experiment_prefix", type=str)
     parser.add_argument("--checkpoint", type=str)
+    parser.add_argument("--param_path", type=str)
     parser.add_argument("--time_range", nargs=2, type=str, default = None)
 
     args = parser.parse_args()
